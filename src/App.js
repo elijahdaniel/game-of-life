9f5aa3c3bc0f971fdfc27fdae35a0e9dba771175
elijahdaniel <e.paminsan@gmail.com>
2020-09-09 09:53:22 -0700

    added instructions

--- src/App.js
+++ src/App.js
@@ -101,41 +101,102 @@ const App = () => {
   }, [speed])
 
   return (
-    <div className='App'>
-      <h1>Game of Life</h1>
-      <button onClick={startGrid}>{running ? 'stop' : 'start'}</button>
-      <button onClick={randomGrid}>random</button>
-      <button onClick={() => setGrid(clearGrid)}>clear</button>|
-      <button className='slow' onClick={() => setSpeed(1000)}>
-        slow
-      </button>
-      <button className='medium' onClick={() => setSpeed(400)}>
-        medium
-      </button>
-      <button className='fast' onClick={() => setSpeed(40)}>
-        fast
-      </button>
-      <div className='whole-grid' style={wholeGrid({ gridCols })}>
-        {grid.map((rows, i) =>
-          rows.map((col, j) => (
-            <div
-              key={`${i}-${j}`}
-              onClick={() => toggleGrid({ i, j })}
-              style={gridColors({ grid, i, j, color })}
-            />
-          ))
-        )}
+    <div className='container'>
+      <div className='App'>
+        <h1>Game of Life</h1>
+        <button onClick={startGrid}>{running ? 'stop' : 'start'}</button>
+        <button onClick={randomGrid}>random</button>
+        <button onClick={() => setGrid(clearGrid)}>clear</button>|
+        <button className='slow' onClick={() => setSpeed(1000)}>
+          slow
+        </button>
+        <button className='medium' onClick={() => setSpeed(400)}>
+          medium
+        </button>
+        <button className='fast' onClick={() => setSpeed(40)}>
+          fast
+        </button>
+        <div className='whole-grid' style={wholeGrid({ gridCols })}>
+          {grid.map((rows, i) =>
+            rows.map((col, j) => (
+              <div
+                key={`${i}-${j}`}
+                onClick={() => toggleGrid({ i, j })}
+                style={gridColors({ grid, i, j, color })}
+              />
+            ))
+          )}
+        </div>
+        <div className='colors'>
+          <span>Change Color:</span>`
+          <div className='green' onClick={() => setColor('green')} />
+          <div className='blue' onClick={() => setColor('blue')} />
+          <div className='orange' onClick={() => setColor('orange')} />
+          <div className='pink' onClick={() => setColor('pink')} />
+          <div className='yellow' onClick={() => setColor('yellow')} />
+          <div className='red' onClick={() => setColor('red')} />
+        </div>
+        <div className='speed'></div>
       </div>
-      <div className='colors'>
-        <span>Change Color:</span>`
-        <div className='green' onClick={() => setColor('green')} />
-        <div className='blue' onClick={() => setColor('blue')} />
-        <div className='orange' onClick={() => setColor('orange')} />
-        <div className='pink' onClick={() => setColor('pink')} />
-        <div className='yellow' onClick={() => setColor('yellow')} />
-        <div className='red' onClick={() => setColor('red')} />
+      <div className='rules'>
+        <h3>Rules of the Game</h3>
+        <p>
+          The universe of the Game of Life is an infinite, two-dimensional
+          orthogonal grid of square cells, each of which is in one of two
+          possible states, live or dead, (or populated and unpopulated,
+          respectively). Every cell interacts with its eight neighbours, which
+          are the cells that are horizontally, vertically, or diagonally
+          adjacent. At each step in time, the following transitions occur:
+        </p>
+        <ol>
+          <li>
+            Any live cell with fewer than two live neighbours dies, as if by
+            underpopulation.
+          </li>
+          <li>
+            Any live cell with two or three live neighbours lives on to the next
+            generation.
+          </li>
+          <li>
+            Any live cell with more than three live neighbours dies, as if by
+            overpopulation.
+          </li>
+          <li>
+            Any dead cell with exactly three live neighbours becomes a live
+            cell, as if by reproduction.
+          </li>
+        </ol>
+        <p>
+          These rules, which compare the behavior of the automaton to real life,
+          can be condensed into the following:
+        </p>
+        <ol>
+          <li>Any live cell with two or three live neighbors survives.</li>
+          <li>Any dead cell with three live neighbours becomes a live cell.</li>
+          <li>
+            All other live cells die in the next generation. Similarly, all
+            other dead cells stay dead.
+          </li>
+        </ol>
+        <p>
+          The initial pattern constitutes the seed of the system. The first
+          generation is created by applying the above rules simultaneously to
+          every cell in the seed; births and deaths occur simultaneously, and
+          the discrete moment at which this happens is sometimes called a tick.
+          Each generation is a pure function of the preceding one. The rules
+          continue to be applied repeatedly to create further generations.
+        </p>
+        <p>
+          To get started, generate a board by clicking the Random button or
+          create a custom grid!
+        </p>
+        <a
+          href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'
+          target='_blank'
+        >
+          Source
+        </a>
       </div>
-      <div className='speed'></div>
     </div>
   )
 }
