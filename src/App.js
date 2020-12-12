bf6613e86bc9fbf2eb37eba494dda9193a773f38
Elijah Paminsan <e.paminsan@gmail.com>
2020-12-11 19:04:09 -0800

    quick start instructions

--- src/App.js
+++ src/App.js
@@ -1,89 +1,92 @@
-import React, { useState, useCallback, useRef } from 'react'
-import produce from 'immer'
+import React, { useState, useCallback, useRef } from 'react';
+import produce from 'immer';
 import Colors from './components/Colors';
 import Controls from './components/Controls';
 import { gridRows, gridCols, directions, clearGrid } from './components/Grid';
 
-import { wholeGrid, gridColors } from './styles/styles'
+import { wholeGrid, gridColors } from './styles/styles';
 
-import './styles/sheet.scss'
+import './styles/sheet.scss';
 
 const App = () => {
-  const [grid, setGrid] = useState(clearGrid)
-  const [running, setRunning] = useState(false)
-  const [color, setColor] = useState('black')
-  const [speed, setSpeed] = useState()
+  const [grid, setGrid] = useState(clearGrid);
+  const [running, setRunning] = useState(false);
+  const [color, setColor] = useState('black');
+  const [speed, setSpeed] = useState();
 
-
-  const runningRef = useRef(running)
-  runningRef.current = running
+  const runningRef = useRef(running);
+  runningRef.current = running;
 
   const randomGrid = () => {
-    const rows = []
-    for (let i = 0;i < gridRows;i++) {
+    const rows = [];
+    for (let i = 0; i < gridRows; i++) {
       rows.push(
         Array.from(Array(gridCols), () => (Math.random() > 0.7 ? 1 : 0))
-      )
+      );
     }
 
-    setGrid(rows)
-  }
+    setGrid(rows);
+  };
 
   const startGrid = () => {
-    setRunning(!running)
+    setRunning(!running);
     if (!running) {
-      runningRef.current = true
-      fireUpGrid()
+      runningRef.current = true;
+      fireUpGrid();
     }
-  }
+  };
 
   const toggleGrid = ({ i, j }) => {
     setGrid(
       produce(grid, gridCopy => {
-        gridCopy[i][j] = grid[i][j] ? 0 : 1
+        gridCopy[i][j] = grid[i][j] ? 0 : 1;
       })
-    )
-  }
+    );
+  };
 
   const fireUpGrid = useCallback(() => {
     if (!runningRef.current) {
-      return
+      return;
     }
 
     setGrid(g => {
       return produce(g, gridCopy => {
-        for (let i = 0;i < gridRows;i++) {
-          for (let j = 0;j < gridCols;j++) {
-            let neighbors = 0
+        for (let i = 0; i < gridRows; i++) {
+          for (let j = 0; j < gridCols; j++) {
+            let neighbors = 0;
             directions.forEach(([x, y]) => {
-              const newI = i + x
-              const newJ = j + y
+              const newI = i + x;
+              const newJ = j + y;
               if (
                 newI >= 0 &&
                 newI < gridRows &&
                 newJ >= 0 &&
                 newJ < gridCols
               ) {
-                neighbors += g[newI][newJ]
+                neighbors += g[newI][newJ];
               }
-            })
+            });
 
             if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][j] = 0
+              gridCopy[i][j] = 0;
             } else if (g[i][j] === 0 && neighbors === 3) {
-              gridCopy[i][j] = 1
+              gridCopy[i][j] = 1;
             }
           }
         }
-      })
-    })
+      });
+    });
 
-    setTimeout(fireUpGrid, speed)
-  }, [speed])
+    setTimeout(fireUpGrid, speed);
+  }, [speed]);
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Conway's Game of Life</h1>
+      <p style={{ padding: '20px 0', color: '#3F51B5' }}>
+        Quick start: Click the Random button to generate a new board then click
+        Start!
+      </p>
       <Controls
         randomGrid={randomGrid}
         startGrid={startGrid}
@@ -92,7 +95,7 @@ const App = () => {
         running={running}
         setGrid={setGrid}
       />
-      <div className='whole-grid' style={wholeGrid({ gridCols })}>
+      <div className="whole-grid" style={wholeGrid({ gridCols })}>
         {grid.map((rows, i) =>
           rows.map((col, j) => (
             <div
@@ -105,7 +108,7 @@ const App = () => {
       </div>
       <Colors setColor={setColor} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
